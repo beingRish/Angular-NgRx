ab014f40dad1c93514a0f9e8ab2267e39e3a4767
beingRish <rishabhjuly11@gmail.com>
2024-07-02 05:32:39 -0700

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BindingComponent } from './binding/binding.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BindingComponent
   ],
   imports: [
     BrowserModule,
